8e7359377701b94c6616f3362fab1c169bd99826
Sanford Wilson <sanford.wilson@gmail.com>
2019-04-12 15:41:16 -0400

    Add field accessors

--- RealEstateSale.java
+++ RealEstateSale.java
@@ -6,9 +6,9 @@ import java.util.Date; import java.util.Calendar;
  * @version 0.5
  */
 public class RealEstateSale {
-  private final String country;
-  private final double price;
-  private final Date date;
+  protected final String country;
+  protected final double price;
+  protected final Date date;
 
   /**
    * Naive constructor.
@@ -22,8 +22,8 @@ public class RealEstateSale {
   /**
    * Data-validating factory for RealEstateSale instances. 
    *
-   * @param country The three-letter country code where the sale occurred.
-   * @param price The fully amount of the transaction.
+   * @param country The 2-letter country code corresponding to the location of the sale.
+   * @param price The amount of the transaction.
    * @param year The year of the transaction.
    * @param month The month of the transaction.
    * @param day Day of the transaction.
@@ -32,10 +32,6 @@ public class RealEstateSale {
    * all fields will be null/zero.
    */
   public static RealEstateSale make(String country, double price, int year, int month, int day) {
-    if (!CurrencyConverter.countryCodes.contains(country)) {
-      country = null;
-      price = 0.0;
-    }
     Calendar cal = Calendar.getInstance();
     cal.set(year, month, day);
     cal.setLenient(false);
@@ -48,7 +44,40 @@ public class RealEstateSale {
       country = null;
       price = 0.0;
     }
+    if (!CurrencyConverter.countryCodes.contains(country)) {
+      country = null;
+      price = 0.0;
+      date = null;
+    }
 
     return new RealEstateSale(country, price, date);
   }
+
+  /**
+   * Accessor for country code.
+   *
+   * @return The 2-letter country code corresponding to the location of the sale.
+   */
+  public String getCountry() {
+    return country;
+  }
+
+  /**
+   * Accessor for sale price
+   *
+   * @return The amount of the transaction.
+   */
+  public double getPrice() {
+    return price;
+  }
+
+  /**
+   * Accessor for sale date.
+   *
+   * @return Date object representing the Month, Day, and Year of the transaction.
+   */
+  public Date getDate() {
+    return date;
+  }
+
 }
