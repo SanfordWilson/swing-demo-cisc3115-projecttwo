f54d3c60867ac66f449f591223e52e134d94de92
Sanford Wilson <sanford.wilson@gmail.com>
2019-04-20 18:29:29 -0400

    Implement simple cell renderer for RealEstateSale JLists

--- src/main/java/projecttwo/ProjectTwo.java
+++ src/main/java/projecttwo/ProjectTwo.java
@@ -3,10 +3,13 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import java.util.ArrayList;
 import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.text.DateFormat;
 
 public class ProjectTwo extends JFrame {
   private JList salesList;
   private JScrollPane scrollPane;
+  private static DateFormat dateFormat = DateFormat.getDateInstance();
 
   private RealEstateSale[] data;
   public static void main(String[] args) {
@@ -20,13 +23,16 @@ public class ProjectTwo extends JFrame {
     scrollPane = new JScrollPane();
     salesList = new JList(data);
 
+    salesList.setCellRenderer(new RealEstateSaleListCellRenderer());
+    add(scrollPane, BorderLayout.CENTER);
+    scrollPane.setViewportView(salesList);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setSize(500, 300);
     setVisible(true);
   }
 
   private void setupData() {
-    data = new RealEstateSale[50];
+    data = new RealEstateSale[20];
     ArrayList<String> codes = CurrencyConverter.countryCodes;
     for (int i = 0; i < 20; i++) {
       data[i] = RealEstateSale.make(
@@ -36,6 +42,19 @@ public class ProjectTwo extends JFrame {
                   (int) (Math.random() * 12),
                   (int) (Math.random() * 31) + 1
               );
+    }
+  }
+  
+  class RealEstateSaleListCellRenderer extends javax.swing.DefaultListCellRenderer {
+    public java.awt.Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+      super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+      if (value instanceof RealEstateSale) {
+        RealEstateSale sale = (RealEstateSale) value;
+        String date = dateFormat.format(sale.getDate());
+
+        setText(String.format("%3s | %s | %f", sale.getCountry(), date, sale.getPrice()));
+      }
+      return this;
     }
   }
 }
